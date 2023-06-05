c60188d89142640c560ac9c0b5f3f0ab26ced8a7
JosefinaRivero <Josefinarivero@gmail.com>
2023-06-05 07:26:14 -0300

    hotel

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const HotelList=() => {
+    return (
+        <section className="HotelList">
+            <Hotel title={firstHotel.title} img={firstHotel.img}/>
+            <Hotel title={secondHotel.title} img={secondHotel.img}/>
+            <Hotel title={tercerHotel.title} img={tercerHotel.img}/>
+            <Hotel title={cuartoHotel.title} img={cuartoHotel.img}/>
+            <Hotel title={quintoHotel.title} img={quintoHotel.img}/>
+            <Hotel title={sextoHotel.title} img={sextoHotel.img}/>
+           
+        </section>
+    )
+}
+
+const firstHotel = {
+    title:'Hotel Miami', 
+    img:"./images/hotel1.jpg",
+}
+const tercerHotel = {
+    title:'hotel la',
+    img:"./images/hotel3.jpg",
+}
+const cuartoHotel = {
+    title:'hotel la',
+    img:"./images/hotel4.jpg",
+}
+const quintoHotel = {
+    title:'hotel la',
+    img:"./images/hotel5.jpg",
+}
+const sextoHotel = {
+    title:'hotel la',
+    img:"./images/hotel6.jpg",
+}
+const secondHotel = {
+    title:'hotel la',
+    img:"./images/hotel2.jpg",
+}
+
+
+
+
+const Hotel = (props) => {
+    console.log(props);
+    const {img, title}=props;
+return(
+    <article className="Hotel">
+       <img src={img} alt={title} ></img> 
+       <h3>{title.toUpperCase()}</h3>
+    </article>
+   
+)
+
+}
+
+
+
+const root=ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<HotelList/>);
+
